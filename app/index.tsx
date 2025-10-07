8cfc7741addab315ef7a3ca00d6e5ed04ce64448
Jk-mern <jkmerndev@gmail.com>
2025-10-07 12:32:08 +0530

    chore: setup project with Tailwind, ESLint, and Prettier

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  console.log('hello world');
+  return (
+    <View>
+      <Text className="text-green-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
