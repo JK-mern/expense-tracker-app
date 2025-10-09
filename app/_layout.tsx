52f25c04db26e4ae11616f786b7acfb2294f125e
Jk-mern <jkmerndev@gmail.com>
2025-10-09 17:52:44 +0530

    feat: login screen frontend

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,43 @@
-import { Stack } from 'expo-router';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts
+} from '@expo-google-fonts/inter';
+import { SplashScreen, Stack } from 'expo-router';
+import { useCallback, useEffect } from 'react';
 import '../global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, fontError] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  useEffect(() => {
+    if (fontError) {
+      console.error('Font loading error:', fontError);
+    }
+  }, [fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false
+      }}
+    />
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './screens/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}'
+  ],
   // @ts-ignore
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
       colors: {
-        primary: '#14F195',
+        primary: '#30e86e',
         'background-light': '#F7F7F7',
         'background-dark': '#121212',
         'gray-light': '#E0E0E0',
         'gray-dark': '#2D2D2D',
-        'text-light': '#121212',
+        'text-light': '#6b7280',
         'text-dark': '#F7F7F7',
         'text-gray-light': '#6B7280',
         'text-gray-dark': '#9CA3AF'
       }
     },
     fontFamily: {
-      display: ['Inter', 'sans-serif']
+      inter: ['Inter_400Regular', 'sans-serif'],
+      'inter-medium': ['Inter_500Medium', 'sans-serif'],
+      'inter-semibold': ['Inter_600SemiBold', 'sans-serif'],
+      'inter-bold': ['Inter_700Bold', 'sans-serif']
     },
     boxShadow: {
       'custom-light':
