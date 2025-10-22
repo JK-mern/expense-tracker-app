ec02633ba0ed723731d41b76559f0e623cdf59fd
Jk-mern <jkmerndev@gmail.com>
2025-10-22 16:31:04 +0530

    feat: login integreation

--- components/login-screen/login-form.tsx
+++ components/login-screen/login-form.tsx
@@ -1,31 +1,111 @@
-import { Link } from 'expo-router';
-import { Pressable, Text, TextInput, View } from 'react-native';
+import { Login, loginSchema } from '@/schemas/auth';
+import { signInWithEmail } from '@/services/auth-service/auth-service';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Link, router } from 'expo-router';
+import { useState, useTransition } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import {
+  ActivityIndicator,
+  Pressable,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
 import GoogleLogo from '../../assets/svgs/google-logo.svg';
 
 export default function LoginForm() {
+  const [isPending, startTransition] = useTransition();
+  const [, setIsErrorToastVisible] = useState<boolean>(false);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Login>({
+    resolver: zodResolver(loginSchema)
+  });
+
+  const onSubmit: SubmitHandler<Login> = (data) => {
+    startTransition(async () => {
+      try {
+        const result = await signInWithEmail(data);
+        if (result) {
+          startTransition(() => {
+            setIsErrorToastVisible(false);
+            router.push('/(auth)/register'); //TODO : change to appropriate route
+          });
+        } else {
+          startTransition(() => {
+            setIsErrorToastVisible(true);
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
   return (
     <View className="justify-center space-y-6">
-      <TextInput
-        className="rounded-xl border border-gray-200 bg-white px-4 py-3 font-inter text-lg focus:border-primary focus:outline-none focus:ring-primary"
-        placeholder="Email"
-        textContentType="emailAddress"
-        keyboardType="email-address"
-      />
-      <TextInput
-        className="rounded-xl border border-gray-200 bg-white px-4 py-3 font-inter text-lg focus:border-primary focus:outline-none focus:ring-primary"
-        placeholder="Password"
-        textContentType="password"
-        secureTextEntry
-      />
+      <View>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <TextInput
+              className="rounded-xl border border-gray-200 bg-white px-4 py-3 font-inter text-lg focus:border-primary focus:outline-none focus:ring-primary"
+              placeholder="Email"
+              textContentType="emailAddress"
+              keyboardType="email-address"
+              value={field.value ?? ''}
+              onChangeText={field.onChange}
+            />
+          )}
+        />
+        {errors.email && (
+          <Text className="ml-2 mt-2 text-sm text-red-600">
+            {errors.email.message}
+          </Text>
+        )}
+      </View>
+
+      <View>
+        <Controller
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <TextInput
+              className="rounded-xl border border-gray-200 bg-white px-4 py-3 font-inter text-lg focus:border-primary focus:outline-none focus:ring-primary"
+              placeholder="Password"
+              textContentType="password"
+              secureTextEntry
+              value={field.value ?? ''}
+              onChangeText={field.onChange}
+            />
+          )}
+        />
+        {errors.password && (
+          <Text className="ml-2 mt-2 text-sm text-red-600">
+            {errors.password.message}
+          </Text>
+        )}
+      </View>
+
       {/* TODO : update with appropriate Link */}
       <Link href={'..'} className="ml-auto font-inter-medium text-primary">
         Forgot password ?
       </Link>
 
-      <Pressable className="rounded-xl bg-primary px-4 py-3">
-        <Text className="text-center font-inter-bold text-base text-white">
-          Login
-        </Text>
+      <Pressable
+        className="rounded-xl bg-primary px-4 py-3"
+        onPress={handleSubmit(onSubmit)}
+      >
+        {isPending ? (
+          <ActivityIndicator color="#ffff" />
+        ) : (
+          <Text className="text-center font-inter-bold text-base text-white">
+            Login
+          </Text>
+        )}
       </Pressable>
       <View className="my-6 flex flex-row items-center justify-between gap-1">
         <View className="flex-1 border-t border-gray-300"></View>
--- schemas/auth/auth.dto.ts
+++ schemas/auth/auth.dto.ts
@@ -1,5 +1,6 @@
 import z from 'zod';
-import { authSchema, createUserSchema } from './auth.schema';
+import { authSchema, createUserSchema, loginSchema } from './auth.schema';
 
 export type AuthSchemaType = z.infer<typeof authSchema>;
 export type CreateUserType = z.infer<typeof createUserSchema>;
+export type Login = z.infer<typeof loginSchema>;
