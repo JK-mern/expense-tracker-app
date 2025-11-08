f09add4b9e7ca004bcccd3876c2e8dfb145a5ed4
Jk-mern <jkmerndev@gmail.com>
2025-11-08 23:14:20 +0530

    feat: update user profile images

--- components/profile-screen/profile-image.tsx
+++ components/profile-screen/profile-image.tsx
@@ -1,8 +1,13 @@
 import { ProfileAvatar } from '@/assets/images';
-import { getImageUrl } from '@/services/image-service/image-service';
+import { useAuth } from '@/providers/auth-provider';
+import {
+  getImageUrl,
+  uploadImage
+} from '@/services/image-service/image-service';
 import { Ionicons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 import { useEffect, useState } from 'react';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { Alert, Image, TouchableOpacity, View } from 'react-native';
 
 type ProfileImageProps = {
   imagePath: string | null;
@@ -10,6 +15,29 @@ type ProfileImageProps = {
 
 const ProfileImage = ({ imagePath }: ProfileImageProps) => {
   const [profileImage, setProfileImage] = useState<string | null>(null);
+  const [newProfileImage, setNewProfileImage] = useState<string | null>(null);
+  const { user } = useAuth();
+
+  const pickImage = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert('Please grant access to library');
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      const uri = result.assets[0].uri;
+      setNewProfileImage(uri);
+    }
+  };
 
   useEffect(() => {
     if (!imagePath) {
@@ -24,13 +52,22 @@ const ProfileImage = ({ imagePath }: ProfileImageProps) => {
     getProfilePicture(imagePath);
   }, [imagePath]);
 
+  useEffect(() => {
+    async function updateProfilePicture() {
+      if (newProfileImage) {
+        await uploadImage({ uri: newProfileImage, userId: user?.id! });
+      }
+    }
+    updateProfilePicture();
+  }, [newProfileImage]);
+
   return (
     <View className="mt-5 items-center">
-      <TouchableOpacity>
+      <TouchableOpacity onPress={pickImage} activeOpacity={0.8}>
         <View className="relative h-32 w-32 items-center justify-center rounded-full bg-[#f5e9e4]">
           {profileImage ? (
             <Image
-              source={{ uri: profileImage }}
+              source={{ uri: newProfileImage ?? profileImage }}
               className="h-32 w-32 rounded-full"
             />
           ) : (
