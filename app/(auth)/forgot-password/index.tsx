93a5e8ea8ba94775b8b2b95933854e3ffafc4824
Jk-mern <jkmerndev@gmail.com>
2025-11-14 23:54:35 +0530

    feat: forgot password screen

---
+++ app/(auth)/forgot-password/index.tsx
@@ -0,0 +1,12 @@
+import ForgotPasswordScreen from '@/screens/forgot-password/forgot-password-screen';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const ForgotPassword = () => {
+  return (
+    <SafeAreaView className="flex-1">
+      <ForgotPasswordScreen />
+    </SafeAreaView>
+  );
+};
+
+export default ForgotPassword;
