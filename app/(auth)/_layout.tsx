acbaed083412436f0da4867f7b78cd65dd2ae047
Jk-mern <jkmerndev@gmail.com>
2025-11-15 23:07:24 +0530

    feat: stack screen protected guard implemented

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,18 +1,6 @@
-import Loader from '@/components/loader/loader';
-import { useAuth } from '@/providers/auth-provider';
-import { Redirect, Stack } from 'expo-router';
+import { Stack } from 'expo-router';
 
 const AuthLayout = () => {
-  const { user, isLoading, userProfileDetails } = useAuth();
-
-  if (isLoading) {
-    return <Loader />;
-  }
-
-  if (user && userProfileDetails && userProfileDetails?.isProfileCompleted) {
-    return <Redirect href={'/(tabs)/home'} />;
-  }
-
   return (
     <Stack
       screenOptions={{
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
+import { useAuth } from '@/providers/auth-provider';
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  return <Redirect href={'/(tabs)/home'} />;
+  return <Redirect href={'/(auth)/login'} />;
 }
