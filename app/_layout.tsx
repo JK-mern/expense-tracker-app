db1d8b5b7c74819aee3f76563425e6d0bf562500
Jk-mern <jkmerndev@gmail.com>
2025-11-29 16:05:08 +0530

    feat: auto toggle status bar icon color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ import { PortalProvider } from '@gorhom/portal';
 import { Toaster } from 'burnt/web';
 import * as NavigationBar from 'expo-navigation-bar';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -48,6 +49,8 @@ export default function RootLayout() {
           <GestureHandlerRootView>
             <PortalProvider>
               <Toaster position="top-center" />
+              <StatusBar style="auto" />
+
               <NavigationLayout />
             </PortalProvider>
           </GestureHandlerRootView>
